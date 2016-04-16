40b22931b4d6a289dc10140a65884dd44d3ced1c
Swapnil Patwa <patwaswapnil@gmail.com>
2016-04-16 16:37:20 +0530

    adfds

--- js/app.js
+++ js/app.js
@@ -10,6 +10,10 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+      if (window.device.platform === 'iOS') {
+    cordova.plugins.iosrtc.registerGlobals();
+    window.OT = cordova.require('cordova-plugin-opentokjs.OpenTokClient');
+  }
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
